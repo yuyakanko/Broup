5fb2143278e43e914c21629d8c5b70d8f71f75e9
yuyakanko <2401010@s.asojuku.ac.jp>
2025-10-31 10:05:00 +0900

    Create homePage.php

---
+++ groupB/homePage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+    <div name="Image.Ad">
+        <img >
+    </div>
+    <?php
+    
+    ?>
+</body>
+</html>

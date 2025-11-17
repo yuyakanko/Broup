3013d047573602d6f2cebe61aaa3aeb8fc3de1de
yuyakanko <2401010@s.asojuku.ac.jp>
2025-11-17 11:53:07 +0900

    修正

--- groupB/データベース.php
+++ groupB/データベース.php
@@ -2,7 +2,7 @@
     const SERVER = 'mysql327.phy.lolipop.lan';
     const DBNAME = 'LAA1607651-system';
     const USER = 'LAA1607651';
-    const PASS = 'asosd2cd';
+    const PASS = 'asosd2cb';
 
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
 ?>

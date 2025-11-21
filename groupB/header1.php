7449d0f45e81aa0c258e5425b7e952a6a4e8c8c0
homess-boop <2401143@s.asojuku.ac.jp>
2025-11-21 10:20:55 +0900

    コミット
    コミット

--- groupB/header1.php
+++ groupB/header1.php
@@ -1,31 +1,26 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  	<link rel="stylesheet" href="css/header1.css">
-</head>
-<body>
-	<nav>
-    	<div class="new-row">
-    		<div class="logo-and-search">
-        		<div class="logo-area">
-            		<img src="image/アイコン1.png" alt="アイコン">
-            		<span>オタグッズ</span>
-        		</div>
-        		<div class="serach-area">
-        			<input type="search" placeholder="探し物">
-        		</div>
-        	</div>
-        	<div class="right-items">
-           		<div class="image">
-               		<img src="image/image.png">
-           		</div>
-         		<div class="maindiv">
-               		<form action="" method="POST">
-               			<button class="button">ログアウト</button>
-           			</form>
-        		</div>
+
+<nav>
+    <div class="new-row">
+		<div class="logo-and-search">
+        	<div class="logo-area">
+        		<img src="image/アイコン1.png" alt="アイコン">
+        		<span>オタグッズ</span>
+    		</div>
+        	<div class="serach-area">
+				<form action="product_search.php" method="POST">
+        			<input type="search" placeholder="探し物" name="search">
+				</form>
     		</div>
     	</div>
-	</nav>
+        <div class="right-items">
+       		<div class="image">
+           		<img src="image/image.png">
+           	</div>
+        	<div class="maindiv">
+           		<form action="" method="POST">
+           			<button class="button">ログアウト</button>
+           		</form>
+        	</div>
+    	</div>
+    </div>
+</nav>
--- groupB/header3.php
+++ groupB/header3.php
@@ -1,49 +1,35 @@
-<?php 
-// homePage.php側で session_start() されている前提
-
-// ログイン状態をチェック
-$is_logged_in = isset($_SESSION['customer']);
-$display_name = $is_logged_in ? htmlspecialchars($_SESSION['customer']['name']) : '';
-$button_link = 'logout.php'; // ログアウトボタンは logout.php を指す
-?>
-
 <nav>
     <div class="top-row">
-        <div class="logo-area">
+    	<div class="logo-area">
             <img src="image/アイコン1.png" alt="アイコン">
-            <span>オタグッズ</span>
-            <input type="search" placeholder="探し物">
-        </div>
+        	<span>オタグッズ</span>
+			<form action="product_search.php" method="POST">
+        		<input type="search" placeholder="探し物" name="search">
+			</form>
+    	</div>
     </div>
-
     <div class="bottom-row">
         <div class="navbar">
-            <ul class="nav-left">
-                <li><a href="homePage.php">ホーム</a></li>
-                <li><a href="#">ゲーム</a></li>
-                <li><a href="#">アニメ</a></li>
-                <li><a href="#">アイドル</a></li>
-                <li><a href="#">ブランド</a></li>
-                <li><button class="sell-button">出品</button></li>
-            </ul>
-        </div>
-        
-        <div class="right-items">
-            <?php if ($is_logged_in): ?>
-                <span class="user-name-display"><?php echo $display_name; ?>さん</span>
-                
-                <div class="image">
-                    <a href="#">
-                        <img src="image/image.png" >
-                    </a>
-                </div>
-                
-                <div class="maindiv">
-                    <a href="<?php echo $button_link; ?>">
-                        <button class="button">ログアウト</button>
-                    </a>
-                </div>
-            <?php endif; ?>
+			<ul class="nav-left">
+				<li><a href="#">ホーム</a></li>
+				<li><a href="#">ゲーム</a></li>
+				<li><a href="#">アニメ</a></li>
+				<li><a href="#">アイドル</a></li>
+				<li><a href="#">ブランド</a></li>
+				<li><button class="sell-button"><a href="product_listing.php">出品</a></button></li>
+  			</ul>
+		</div>
+    	<div class="right-items">
+            <div class="image">
+        		<a href="#">
+            		<img src="image/image.png" >
+                </a>
+            </div>
+        	<div class="maindiv">
+            	<a href="#">
+                    <button class="button">ログアウト</button>
+                </a>
+            </div>
         </div>
     </div>
 </nav>
